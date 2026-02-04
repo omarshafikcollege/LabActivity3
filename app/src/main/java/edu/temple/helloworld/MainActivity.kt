4ae887749e2513dade526aab04c4884170d35032
omarshafik12 <omarsaidshafik@gmail.com>
2026-02-04 15:32:48 -0500

    feature fixed

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -18,9 +18,16 @@ class MainActivity : AppCompatActivity() {
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
 
-        
+
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            val nameEditText = findViewById<EditText>(R.id.nameEditText)
+            val name = nameEditText.text.toString().trim()
+
+            if (name.isEmpty()) {
+                nameEditText.error = "Please enter your name"
+            } else {
+                displayTextView.text = "Hello, $name"
+            }
         }
 
 

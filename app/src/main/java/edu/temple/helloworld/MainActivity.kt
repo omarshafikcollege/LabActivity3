1ef9370c60e7b2a2364cd77b460904150af23da6
omarshafik12 <omarsaidshafik@gmail.com>
2026-02-04 15:34:08 -0500

    need to push

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
                 displayTextView.text = "Hello, $name"
             }
         }
-
+        //just need this comment for the commit
 
     }
 }
